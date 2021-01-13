628196d588bcf1069bf1a1f943133e0babfcda10
t-nakamura3116 <t-nakamura@cmind-co.jp>
2021-01-13 19:03:49 +0900

    ユーザー認証

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin'], function() {
+Route::group(['prefix' => 'admin' , 'middleware' => 'auth'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
-});
-
-Route::group(['prefix' => 'admin'], function() {
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
+
+
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
