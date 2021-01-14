7f7a0950a170ab3f8b46881357a2b76a5b0993b5
t-nakamura3116 <t-nakamura@cmind-co.jp>
2021-01-14 19:35:41 +0900

    ニュース投稿画面

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -10,7 +10,43 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>プロフィール</h2>
+                <h2>Myプロフィール</h2>
+                <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+
+                    @if (count($errors) > 0)
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li>{{ $e }}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+                    <div class="form-group row">
+                        <label class="col-md-2" for="name">氏名</label>
+                        <div class="col-md-10">
+                            <input type="name" class="form-control" name="name" value="{{ old('name') }}">
+                        </div>
+                    </div>
+                    <div>
+                        <input type="radio" name="q1" value="男性"> 男性
+                    </div>
+                    <div>
+                    <input type="radio" name="q1" value="女性"> 女性
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="body">趣味</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="body" rows="5">{{ old('body') }}</textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-md-2" for="body">自己紹介欄</label>
+                        <div class="col-md-10">
+                            <textarea class="form-control" name="body" rows="10">{{ old('body') }}</textarea>
+                        </div>
+                    </div>
+                    {{ csrf_field() }}
+                    <input type="submit" class="btn btn-primary" value="更新">
+                </form>
             </div>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,11 @@ Route::get('/', function () {
 
 Route::group(['prefix' => 'admin' , 'middleware' => 'auth'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
+    Route::post('news/create', 'Admin\NewsController@create');
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::post('profile/create', 'Admin\ProfileController@create');
+    Route::get('profile/edit', 'Admin\ProfileController@update');
 });
 
 
